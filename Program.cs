64307947a16fa72cb2ef15f9c2c23450e2b153b1
CnRockwang <xin265755@hotmail.com>
2015-03-30 15:44:32 +0800

    test
    1

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Vkey
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        
+
+
     }
 }
