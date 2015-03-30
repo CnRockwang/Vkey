216f163034e372ca7194e2d507e6db0b97f8d76c
CnRockwang <xin265755@hotmail.com>
2015-03-30 15:50:09 +0800

    test
    1

--- Program.cs
+++ Program.cs
@@ -20,14 +20,14 @@ namespace Vkey
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            Game.OnUpdate += Game_OnGameUpdate;
+            //Game.OnUpdate += Game_OnGameUpdate;
 
-            _menu = new Menu("Rockwang_Virtual_KeyDown", "vkd", true);
-            _menu.AddSubMenu(new Menu("Vkeydown_keybins", "vkd_kb"));
-            _menu.SubMenu("vkd_kb").AddSubMenu(new Menu("Key_one", "keyone"));
-            _menu.SubMenu("vkd_kb").SubMenu("key1").AddItem(new MenuItem("clckkey1", "key_one").SetValue(new KeyBind(57, KeyBindType.Press)));
-            _menu.SubMenu("vkd_kb").AddSubMenu(new Menu("Key_one", "vkey1"));
-            _menu.AddToMainMenu();
+            //_menu = new Menu("Rockwang_Virtual_KeyDown", "vkd", true);
+            //_menu.AddSubMenu(new Menu("Vkeydown_keybins", "vkd_kb"));
+            //_menu.SubMenu("vkd_kb").AddSubMenu(new Menu("Key_one", "keyone"));
+            //_menu.SubMenu("vkd_kb").SubMenu("key1").AddItem(new MenuItem("clckkey1", "key_one").SetValue(new KeyBind(57, KeyBindType.Press)));
+            //_menu.SubMenu("vkd_kb").AddSubMenu(new Menu("Key_one", "vkey1"));
+            //_menu.AddToMainMenu();
 
 
 
